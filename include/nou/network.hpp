b8831aa42cdd89611d9eaa89944bad8e7f0a56d7
ken-963f <153351383+ken-963f@users.noreply.github.com>
2023-12-29 18:18:13 +0900

    [feat] #9 implment constructor in network class (#24)
    close #9

--- include/nou/network.hpp
+++ include/nou/network.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <concepts>
+#include <tuple>
+#include <type_traits>
 
 #include "nou/concepts/layer.hpp"
 #include "nou/layer/input_layer.hpp"
@@ -14,6 +16,33 @@ template <complete_layer InputLayer, layer... Layers>
            std::invocable<decltype(connect_layers<InputLayer, Layers...>),
                           InputLayer, Layers...> &&
            (sizeof...(Layers) >= 1)
-class network final {};
+class network final {
+ public:
+  // Member Type
+  using layers_type =
+      std::invoke_result_t<decltype(connect_layers<InputLayer, Layers...>),
+                           InputLayer, Layers...>;
+  using value_type = decltype(std::apply(
+      [](auto&&... layers) { return std::tuple{layers.value()...}; },
+      layers_type{}));
+
+  // Constructor
+  network() = default;
+
+  [[nodiscard]] explicit constexpr network(const InputLayer&& input_layer,
+                                           Layers&&... layers)
+      : layers_{connect_layers(input_layer, std::forward<Layers>(layers)...)} {}
+
+  // Getter/Setter
+  [[nodiscard]] constexpr auto value() const noexcept -> value_type {
+    return std::apply(
+        [](auto&&... layers) { return std::tuple{layers.value()...}; },
+        layers_);
+  }
+
+ private:
+  // Member variable
+  layers_type layers_{};
+};
 
 }  // namespace nou
--- test/ut/network.cc
+++ test/ut/network.cc
@@ -8,21 +8,41 @@
 
 namespace mock {
 
-template <std::floating_point RealType, std::size_t OutputSize,
-          std::size_t InputSize>
+template <std::floating_point RealType, std::size_t InputSize,
+          std::size_t OutputSize>
 struct complete_layer final {
+  // Member Type
   using real_type = RealType;
   using size_type = std::size_t;
+  using value_type = real_type;
 
+  // Static Member
   static constexpr size_type input_size = InputSize;
   static constexpr size_type output_size = OutputSize;
+
+  // Constructor
+  complete_layer() = default;
+  [[nodiscard]] explicit constexpr complete_layer(real_type value)
+      : value_{value} {}
+
+  // Getter/Setter
+  [[nodiscard]] constexpr auto value() const noexcept -> real_type {
+    return value_;
+  }
+
+ private:
+  real_type value_{};
 };
 
 template <std::size_t OutputSize>
 struct incomplete_layer final {
+  // Member Type
   using size_type = std::size_t;
+
+  // Static Member
   static constexpr size_type output_size = OutputSize;
 
+  // Public Functions
   template <nou::complete_layer PrevLayer>
   constexpr auto make_complete_layer() noexcept {
     return complete_layer<typename PrevLayer::real_type, PrevLayer::output_size,
@@ -45,5 +65,21 @@ auto main() -> int {
         std::is_nothrow_default_constructible_v<nou::network<
             nou::input_layer<RealType, 1UZ>, mock::incomplete_layer<2UZ>,
             mock::incomplete_layer<3UZ>>>);
+  } | test_value;
+
+  "constructor"_test = []<std::floating_point RealType>() {
+    constexpr auto value1 = nou::network{nou::input_layer<RealType, 1UZ>{},
+                                         mock::incomplete_layer<2UZ>{},
+                                         mock::incomplete_layer<3UZ>{}}
+                                .value();
+    static_assert(std::get<0>(value1) == RealType{});
+    static_assert(std::get<1>(value1) == RealType{});
+
+    constexpr auto value2 =
+        nou::network{nou::input_layer<RealType, 1UZ>{},
+                     mock::complete_layer<RealType, 1UZ, 2UZ>{1.0},
+                     mock::complete_layer<RealType, 2UZ, 3UZ>{2.0}}.value();
+    static_assert(std::get<0>(value2) == RealType{1.0});
+    static_assert(std::get<1>(value2) == RealType{2.0});
   } | test_value;
 }
